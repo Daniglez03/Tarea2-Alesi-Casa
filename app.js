0ffd4a7cb6957a29c96fdf74358b5b78df5a4817
Daniglez03 <101868574+Daniglez03@users.noreply.github.com>
2022-09-29 23:28:34 +0100

    Commit final

--- app.js
+++ app.js
@@ -1,65 +1,78 @@
 const args = process.argv.slice(2)
 
-let arrNum = []
-const loadNumber = () => {
-    args.slice(1).forEach(value => {
-        let number = Number(value.substring(5))
-        arrNum.push(number)
-    })
-}
-loadNumber()
+if (args.length > 0) {
+    let arrNum = []
+    const loadNumber = () => {
+        args.slice(1).forEach(value => {
+            if (value.substring(0, 5) === "--op=") {
+                let number = Number(value.substring(5))
+                if (!isNaN(number) && number !== 0) {
+                    arrNum.push(number)
+                }
+            }
+        })
+    }
+    loadNumber()
 
-let signo = args[0].substring(12)
-
-let resultado = {
-    operator: signo,
-    ops: arrNum,
-    res: 0
-}
-
-// Recordar aprender.
-//función reductora sobre cada elemento de un array, devolviendo como resultado un único valor.
-
-// hacer metodo para ignorar los ops no válidos
-
-switch (args[0]) {
-    case "--operation=/":
-        if (arrNum.length >= 2) {
-            const totalDiv = arrNum.reduce((p, c) => p / c)
-            resultado.res = totalDiv
-            console.log("operación : ",resultado);
-        } else {
-            console.log("ERROR: El número de operandos debe ser mayor que 1");
-        }
-        break;
-    case "--operation=*":
-        if (arrNum.length >= 2) {
-            const totalMult = arrNum.reduce((p, c) => p * c)
-            resultado.res = totalMult
-            console.log("operación : ",resultado);
-        }else {
-            console.log("ERROR: El número de operandos debe ser mayor que 1");
-        }
-        break
-    case "--operation=^":
-        if (arrNum.length > 1 && arrNum.length < 3) {
-            const totalPotencia = arrNum[0] ** arrNum[1]
-            resultado.res = totalPotencia
-            console.log("operación : ",resultado);
-        } else {
-            console.log("ERROR: El número de operandos debe ser 2");
-        }
-        break;
-    case "--operation=@":
-        if (arrNum.length === 1) {
-            const totalRaiz = Math.sqrt(arrNum[0])
-            resultado.res = totalRaiz
-            console.log("operación : ",resultado);
-        } else {
-            console.log("ERROR: El número de operandos debe ser 1");
-        }
-        break
-    default:
-        console.log("ERROR: Parámetros erróneos");
-        break;
+    let signo = args[0].substring(12)
+    
+    let resultado = {
+        operator: signo,
+        ops: arrNum,
+        res: 0
+    }
+    switch (args[0]) {
+        case "--operation=/":
+            if (arrNum.length >= 2) {
+                //Más resumido pero no se si se puede utilizar reducir
+                //const totalDiv = arrNum.reduce((p, c) => p / c)
+                let total = 0
+                total = arrNum[0] / arrNum[1]
+                arrNum.slice(2).forEach(element => {
+                    total /= element
+                });
+                resultado.res = total
+                console.log("operación : ", resultado);
+            } else {
+                console.log("ERROR: El número de operandos debe ser mayor que 1");
+            }
+            break;
+        case "--operation=*":
+            if (arrNum.length >= 2) {
+                //Más resumido pero no se si se puede utilizar reducir
+                //const totalMult = arrNum.reduce((p, c) => p * c)
+                let mult = 1
+                arrNum.forEach(element => {
+                    mult *= element
+                });
+                resultado.res = mult
+                console.log("operación : ", resultado);
+            } else {
+                console.log("ERROR: El número de operandos debe ser mayor que 1");
+            }
+            break
+        case "--operation=^":
+            if (arrNum.length > 1 && arrNum.length < 3) {
+                const totalPotencia = arrNum[0] ** arrNum[1]
+                resultado.res = totalPotencia
+                console.log("operación : ", resultado);
+            } else {
+                console.log("ERROR: El número de operandos debe ser 2");
+            }
+            break;
+        case "--operation=@":
+            if (arrNum.length === 1) {
+                const totalRaiz = Math.sqrt(arrNum[0])
+                resultado.res = totalRaiz
+                console.log("operación : ", resultado);
+            } else {
+                console.log("ERROR: El número de operandos debe ser 1");
+            }
+            break
+        default:
+            console.log("ERROR: Parámetros erróneos");
+            break;
+    }
+} else {
+    console.log("ERROR: No hay argumentos");
 }
